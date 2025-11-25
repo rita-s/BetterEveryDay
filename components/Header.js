13e8e955c99f8428a5057344c94e0a6fa8f5983c
Rita <rita.s.github@gmail.com>
2025-11-25 10:57:43 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
+import Navigation from './Navigation';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
-      <p className="text-2xl text-center dark:text-white">
-        <Link href="/">{name}</Link>
-      </p>
+      <Link href="/">
+        <h1 className="mb-8 text-2xl font-bold text-center dark:text-white">
+          {name}
+        </h1>
+      </Link>
+      <Navigation />
     </header>
   );
 }
