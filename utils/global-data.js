a7fd6f4235652b2d8bbebc5c7e38d4aa5c17e033
Rita <rita.s.github@gmail.com>
2025-11-25 10:49:35 +0000

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,15 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'betterevery.one';
+
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Better Every One';
+
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Small steps. Real progress. No bullshit.';
 
   return {
     name,
